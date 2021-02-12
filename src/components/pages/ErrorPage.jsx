de950f3f560c43157c6d2f42f8ecc164192a2469
wachcio <wachcio@poczta.fm>
2021-02-12 15:31:18 +0100

    remove eslintchache

--- src/components/pages/ErrorPage.jsx
+++ src/components/pages/ErrorPage.jsx
@@ -12,8 +12,8 @@ const ErrorPage = () => {
     <div className="flex flex-center h-screen w-screen justify-center content-center">
       <WrapperBackroundWhite>
         <div className="flex flex-col flex-center justify-center content-center items-center">
-          <div className="flex flex-col flex-center justify-center content-center items-center py-4">
-            <h1 className="text-6xl font-bold py-2">404</h1>
+          <div className="flex flex-col flex-center justify-center content-center items-center py-4 text-xl">
+            <h1 className="text-8xl font-bold py-2">404</h1>
             <p>Niestety nie znaleziono strony.</p>
           </div>
           <img className="max-w-52 w-52" src={img404} alt="Error 404" />
