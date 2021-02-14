42f05212d439eeb42abc3e55e0c7344797866969
wachcio <wachcio@poczta.fm>
2021-02-15 00:06:05 +0100

    gallery working. css bad

--- src/components/molecules/MushroomImageList.jsx
+++ src/components/molecules/MushroomImageList.jsx
@@ -1,6 +1,14 @@
+/* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
 import React, { useEffect } from 'react';
 import useStateWithLabel from '../../hooks/useStateWhitLabel';
-import MushroomImage from '../atoms/MushroomImage';
+// import MushroomImage from '../atoms/MushroomImage';
+
+import 'photoswipe/dist/photoswipe.css';
+import 'photoswipe/dist/default-skin/default-skin.css';
+
+// eslint-disable-next-line import/order
+import { Gallery, Item } from 'react-photoswipe-gallery';
 
 function MushroomImageList({ mushroom: { images, id, polishName } }) {
   const [imagesLinks, setImageslinks] = useStateWithLabel('imagesLinks', []);
@@ -19,16 +27,47 @@ function MushroomImageList({ mushroom: { images, id, polishName } }) {
   return (
     <>
       <h1>Galeria:</h1>
-      <div className="flex flex-col sm:flex-row sm:flex-wrap w-100 justify-center items-center">
-        {imagesLinks != null &&
-          imagesLinks.map((v, i) => (
-            <MushroomImage
-              patch={v}
-              key={v}
-              imageTitle={`${polishName} - zdjęcie ${i + 1}`}
-            />
-          ))}
-      </div>
+      <Gallery>
+        <div
+          style={{
+            display: 'grid',
+            gridTemplateColumns: '1fr 1fr 1fr 1fr',
+            gridTemplateRows: '1fr',
+            gridGap: 12,
+          }}
+        >
+          {imagesLinks != null &&
+            imagesLinks.map((v, i) => (
+              <Item
+                original={v}
+                thumbnail={v}
+                // width="100%"
+                // height="100%"
+                // minWidth="1024"
+                // minHheight="768"
+                width="1024"
+                height="768"
+                key={v}
+              >
+                {({ ref, open }) => (
+                  <img
+                    style={{
+                      cursor: 'pointer',
+                      objectFit: 'cover',
+                      width: 'auto',
+                      maxHeight: '100%',
+                    }}
+                    ref={ref}
+                    onClick={open}
+                    src={v}
+                    imageTitle={`${polishName} - zdjęcie ${i + 1}`}
+                    alt={`${polishName} - zdjęcie ${i + 1}`}
+                  />
+                )}
+              </Item>
+            ))}
+        </div>
+      </Gallery>
     </>
   );
 }
